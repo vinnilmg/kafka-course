81e35bb63a826415db49cec171b2340d44f700b6
vinnilmg <vini.delimagomes@gmail.com>
2024-01-05 15:09:21 -0300

    consumer groups

--- str-consumer/src/main/java/com/vinnilmg/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/vinnilmg/strconsumer/listeners/StrConsumerListener.java
@@ -8,9 +8,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class StrConsumerListener {
 
+    @KafkaListener(groupId = "group-0", topics = "${kafka.topic-name}", containerFactory = "strContainerFactory")
+    public void listener1(String message) {
+        log.info("[listener 1] Mensagem recebida: {}", message);
+    }
+
     @KafkaListener(groupId = "group-1", topics = "${kafka.topic-name}", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Mensagem recebida: {}", message);
+    public void listener2(String message) {
+        log.info("[listener 2] Mensagem recebida: {}", message);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "${kafka.topic-name}", containerFactory = "strContainerFactory")
+    public void listener3(String message) {
+        log.info("[listener 3] Mensagem recebida: {}", message);
     }
 
 }
