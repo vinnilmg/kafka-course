3c66760750a317962fe0a98fe4d8beb9c285700f
vinnilmg <vini.delimagomes@gmail.com>
2024-01-05 12:22:35 -0300

    add callback function to sendMessage function

--- str-producer/src/main/java/com/vinnilmg/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/vinnilmg/strproducer/services/StringProducerService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import static java.util.Objects.nonNull;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -18,7 +20,16 @@ public class StringProducerService {
 
     public void sendMessage(String message) {
         log.info("Enviando mensagem: {}", message);
-        kafkaTemplate.send(topicName, message);
+        kafkaTemplate.send(topicName, message).addCallback(
+                success -> {
+                    if (nonNull(success)) {
+                        log.info("Mensagem enviada com sucesso.");
+                        log.info("Partition: {}", success.getRecordMetadata().partition());
+                        log.info("Offset: {}", success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Erro ao enviar mensagem: {}", error.getMessage(), error)
+        );
     }
 
 }
