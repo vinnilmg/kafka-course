c3704d437848b597a5d435ee161b64421bbc2a56
vinnilmg <vini.delimagomes@gmail.com>
2024-02-03 19:39:14 -0300

    add logs to error handler

--- str-consumer/src/main/java/com/vinnilmg/strconsumer/exceptions/ErrorCustomHandler.java
+++ str-consumer/src/main/java/com/vinnilmg/strconsumer/exceptions/ErrorCustomHandler.java
@@ -1,7 +1,6 @@
 package com.vinnilmg.strconsumer.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.Consumer;
 import org.springframework.kafka.listener.KafkaListenerErrorHandler;
 import org.springframework.kafka.listener.ListenerExecutionFailedException;
 import org.springframework.messaging.Message;
@@ -14,6 +13,12 @@ public class ErrorCustomHandler implements KafkaListenerErrorHandler {
     @Override
     public Object handleError(Message<?> message, ListenerExecutionFailedException e) {
         log.info("EXCEPTION_HANDLER ::: Erro capturado.");
+        log.info("Payload: {}", message.getPayload());
+        log.info("Headers: {}", message.getHeaders());
+        log.info("Offset: {}", message.getHeaders().get("kafka_offset"));
+        log.info("Partition: {}", message.getHeaders().get("kafka_receivedPartitionId"));
+        log.info("GroupId: {}", message.getHeaders().get("kafka_groupId"));
+        log.info("ExceptionMessage: {}", e.getMessage());
         return null;
     }
 
