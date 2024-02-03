9b6d74f76664fa8185cba65722f0450464aaa6eb
vinnilmg <vini.delimagomes@gmail.com>
2024-02-03 18:42:32 -0300

    add custom annotation to kafka listener

--- str-consumer/src/main/java/com/vinnilmg/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/vinnilmg/strconsumer/listeners/StrConsumerListener.java
@@ -1,36 +1,24 @@
 package com.vinnilmg.strconsumer.listeners;
 
+import com.vinnilmg.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class StrConsumerListener {
 
-    // Configurado para escutar apenas a partição 0
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-                @TopicPartition(topic = "${kafka.topic-name}", partitions = {"0"})
-            },
-            containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void listener1(String message) {
         log.info("[listener 1] Mensagem recebida: {}", message);
     }
 
-    // Configurado para escutar apenas a partição 1
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-                    @TopicPartition(topic = "${kafka.topic-name}", partitions = {"1"})
-            },
-            containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void listener2(String message) {
         log.info("[listener 2] Mensagem recebida: {}", message);
     }
 
-    // Configurado para escutar as partições 0 e 1
-    @KafkaListener(groupId = "group-2", topics = "${kafka.topic-name}", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-2")
     public void listener3(String message) {
         log.info("[listener 3] Mensagem recebida: {}", message);
     }
